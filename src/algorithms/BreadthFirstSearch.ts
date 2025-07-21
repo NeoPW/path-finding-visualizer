ed92ed2300c5ea5230d65c5a0437343554249bbb
Paul Walther <paul.walther@onexip.com>
2025-07-21 14:59:56 +0200

    added pop effect for wall cells and changed bfs to yield after batch

--- src/algorithms/BreadthFirstSearch.ts
+++ src/algorithms/BreadthFirstSearch.ts
@@ -14,6 +14,7 @@ export function* breadthFirstSearchSteps(
   visited: boolean[][];
   parent: (Point | null)[][];
   found: boolean;
+  layer: number;
 }, void, unknown> {
   const numRows = grid.length;
   const numCols = grid[0].length;
@@ -22,32 +23,40 @@ export function* breadthFirstSearchSteps(
 
   const queue: Point[] = [start];
   visited[start[0]][start[1]] = true;
+  let layer = 0;
 
   while (queue.length > 0) {
-    const current = queue.shift()!;
-    const [row, col] = current;
-    if (row === end[0] && col === end[1]) {
-      yield { current, visited, parent, found: true };
-      return;
-    }
-    yield { current, visited, parent, found: false };
-    const neighbors: Point[] = [
-      [row - 1, col],
-      [row + 1, col],
-      [row, col - 1],
-      [row, col + 1],
-    ];
-    for (const [nRow, nCol] of neighbors) {
-      if (
-        nRow >= 0 && nRow < numRows &&
-        nCol >= 0 && nCol < numCols &&
-        !visited[nRow][nCol] &&
-        grid[nRow][nCol] !== GridCellType.WALL
-      ) {
-        queue.push([nRow, nCol]);
-        visited[nRow][nCol] = true;
-        parent[nRow][nCol] = [row, col];
+    const currentLayerSize = queue.length;
+    let found = false;
+    let lastCurrent: Point = start;
+    for (let i = 0; i < currentLayerSize; i++) {
+      const current = queue.shift()!;
+      lastCurrent = current;
+      const [row, col] = current;
+      if (row === end[0] && col === end[1]) {
+        yield { current, visited, parent, found: true, layer };
+        return;
+      }
+      const neighbors: Point[] = [
+        [row - 1, col],
+        [row + 1, col],
+        [row, col - 1],
+        [row, col + 1],
+      ];
+      for (const [nRow, nCol] of neighbors) {
+        if (
+          nRow >= 0 && nRow < numRows &&
+          nCol >= 0 && nCol < numCols &&
+          !visited[nRow][nCol] &&
+          grid[nRow][nCol] !== GridCellType.WALL
+        ) {
+          queue.push([nRow, nCol]);
+          visited[nRow][nCol] = true;
+          parent[nRow][nCol] = [row, col];
+        }
       }
     }
+    yield { current: lastCurrent, visited, parent, found: false, layer };
+    layer++;
   }
 } 
